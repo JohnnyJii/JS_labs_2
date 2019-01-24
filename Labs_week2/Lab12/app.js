3f53bb4fbf8a2b728b0826eff29ff5c4afdfc510
Jukka Juntunen <jukka.juntunen@metropolia.fi>
2019-01-24 11:00:22 +0200

    lab12

--- Labs_week2/Lab12/app.js
+++ Labs_week2/Lab12/app.js
@@ -1,17 +1,35 @@
-const intRange = (low, high, step) => {
-  const newArray = [];
-  for ( let i = low; i <= high; i = i + step ) {
-    pickRnd(newArray, 7) {
-      rnd = randomArray(0, arr.lenght -1);
-      picked = new Set();
-        while (picked.size < 7) {
-          picked.add(rnd());
-        }
-    }
-    newArray.push(i);
+Array.prototype.swap = (function() {
+  let i = 0, j = 1;
+  try {
+    [i, j] = [j, i];
   }
-  return newArray;
+  catch (e) {
+  }
+  if (i) {
+    return function(i, j) {
+      [this[i], this[j]] = [this[j], this[i]];
+      return this;
+    };
+  } else {
+    return function(i, j) {
+      let temp = this[i];
+      this[i] = this[j];
+      this[j] = temp;
+      return this;
+    };
+  }
+})();
+
+Array.prototype.pick = function(n) {
+  if (!n || !this.length) return [];
+  let i = Math.floor(this.length * Math.random());
+  return this.splice(i, 1).concat(this.pick(n - 1));
 };
 
-console.log(intRange(1, 40, 1));
+function pick(low, high, step) {
+  let a = [], i = high;
+  while (i >= low) a.push(i--);
+  return a.pick(step);
+}
 
+console.log(pick(1, 100, 8));
